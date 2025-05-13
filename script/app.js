4944a3feadb505cd69cfadbb11b5d095ebd77344
Your Name <you@example.com>
2025-05-13 11:42:59 +0000

    mana

--- script/app.js
+++ script/app.js
@@ -1,4 +1,4 @@
 Alpine.store("settings", {
-    apiBaseUrl:"http://172.17.100.14:3383/default/api/",  
+    apiBaseUrl:"http://172.17.100.14:3368/schoenke3/api/",  
     appName: "Ecommerce",  
 });
